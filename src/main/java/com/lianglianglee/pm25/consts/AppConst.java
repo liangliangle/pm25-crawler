968fa3d52bb8507b91e2b6c5fb545c42ca9e116b
lianglianglee <liangliang_li@fengzhiding.com>
2018-10-05 11:13:07 +0800

    去除 城市，监测点

--- src/main/java/com/lianglianglee/pm25/consts/AppConst.java
+++ src/main/java/com/lianglianglee/pm25/consts/AppConst.java
@@ -18,12 +18,6 @@ public class AppConst {
   private static String date;
 
 
-  private static Map<String, String> cityMap = new ConcurrentHashMap<>();
-
-
-  private static Map<String, String> stationMap = new ConcurrentHashMap<>();
-
-
   public synchronized static String getDate() {
     if (null == date) {
       date = new DataDao().getNewDate();
@@ -35,35 +29,8 @@ public class AppConst {
     date = newdate;
   }
 
-  public synchronized static void checkCity(String city) {
-    String oldCity = cityMap.get(city);
-    if (null == oldCity) {
-      cityMap.put(city, city);
-      new DataDao().insertCity(city);
-    }
-
-  }
-
-  public synchronized static void checkDevice(String station) {
-    String oldStation = stationMap.get(station);
-    if (null == oldStation) {
-      cityMap.put(station, station);
-      new DataDao().insertDevice(station);
-    }
-
-  }
-
 
   public static void build() {
-    List<String> citys = new DataDao().getAllCity();
-    citys.forEach(city -> {
-      cityMap.put(city, city);
-    });
-    List<String> stations = new DataDao().getDevice();
-    stations.forEach(station -> {
-              stationMap.put(station, station);
-            }
-    );
     String date = new DataDao().getNewDate();
     setDate(date);
   }
